a30b660180106b32e3a290558a821db9795df22f
Eric Daniele <ericdaniele34@gmail.com>
2022-10-12 11:09:13 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+const app = express();
+
+app.set('port', process.env.PORT);
+app.listen(app.get('port'), () => {
+    console.log(`Server listening on port ${app.get('port')}`)
+});
