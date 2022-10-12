af9e36c16620570367f371d6ac949461bd3c031f
Eric Daniele <ericdaniele34@gmail.com>
2022-10-12 17:58:37 -0300

    post schema

---
+++ api/src/models/post.ts
@@ -0,0 +1,42 @@
+import { Document, model, Schema } from "mongoose";
+import userSchema from "./user";
+
+export interface Ipost extends Document {
+  enabled: boolean;
+  IDauthor: string;
+  likes?: number;
+  postType: string;
+  content: string;
+  image: string;
+  comments?: string;
+}
+
+const postSchema = new Schema({
+  enabled: {
+    type: Boolean,
+    required: true,
+  },
+  author: userSchema,
+  likes: {
+    type: Number,
+    required: false,
+  },
+  postType: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    Array,
+    required: false,
+  },
+});
+
+export default model<Ipost>('post', postSchema);
