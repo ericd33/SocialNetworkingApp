f1edbc2fe804fdac8902c9c77e42ebd16a8acc8a
Fede Rosales <federico.rosales.salva@gmail.com>
2022-10-14 17:08:26 -0300

    setPostEnDis

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -69,3 +69,38 @@ export const findUserAndGetAllUser = async (req: Request, res: Response) => {
 };
 
 
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id, action } = req.query;
+
+  try {
+    if (id) {
+      const user = await userSchema.findOne({ _id: id });
+
+      switch (action) {
+        case "disable":
+          if (user.enabled) {
+            await userSchema.updateOne({ _id: id },{ enabled: false }
+            );
+            res.status(200).send("User deleted successfully.");
+          } else {
+            res.status(400).send("User is already deleted.");
+          }
+          break;
+          case "enable":
+            if (!user.enabled) {
+              await userSchema.updateOne({ _id: id },{ enabled: true }
+              );
+              res.status(200).send("User re-enabled successfully.");
+            } else {
+              res.status(400).send("User is already deleted.");
+            }
+          break;
+        default:
+          res.status(400).send('Invalid action request.')
+          break;
+      }
+    }
+  } catch (err) {
+    res.status(404).send(err);
+  }
+}
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addUser, findUserAndGetAllUser,  } from "../controllers/user.controller";
+import { addUser, findUserAndGetAllUser,  deleteUser} from "../controllers/user.controller";
 const router = Router()
 
 router.route('')
     .post(addUser)
     .get(findUserAndGetAllUser)
+    .put(deleteUser)
 
 export default router;
