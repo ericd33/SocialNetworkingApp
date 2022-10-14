a73cafcddbe28ee2ac4b4e57421f62ea2e087c2f
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-10-14 13:18:32 -0300

    fix: add cors rules

--- api/src/controllers/events.controllers.ts
+++ api/src/controllers/events.controllers.ts
@@ -1,62 +1,53 @@
 import { Request, Response } from "express";
-const eventSchema = require('../models/event')
-
+const eventSchema = require("../models/event");
 
 export const addEvent = async (req: Request, res: Response) => {
-    const {
-        name,
-        date,
-        hour,
-        enabled,
-        content,
-        image,
-        location
-    } = req.body
-    try{
-    let event = await new eventSchema()
-    if(name.length && date.length && content.length && location.length){
-        event.name = name
-        event.date = date
-        event.hour = hour
-        event.enabled = enabled
-        event.content = content
-        event.image = image
-        event.location = location
-        event.enabled = false
-        await event.save(function (err:any, event:any) {
-            if (err) {
-                res.send(err);
-            }
-                console.log(event);
-            })
-    res.status(200).send("new comment");
-    }}catch(e){
-        res.status(400).send(e)
-    }
-}
-
-
-
-export const findEvent = async(req: Request, res: Response) => {
-    const { name } = req.query
-    try{
-        if(name){
-        const event = await eventSchema.find({ name: { $regex: name, $options: 'ig'}});
-        if(event.length){
-            res.status(200).send(event);
-            return
+  const { name, date, hour, enabled, content, image, location } = req.body;
+  try {
+    let event = await new eventSchema();
+    if (name.length && date.length && content.length && location.length) {
+      event.name = name;
+      event.date = date;
+      event.hour = hour;
+      event.enabled = enabled;
+      event.content = content;
+      event.image = image;
+      event.location = location;
+      event.enabled = false;
+      await event.save(function (err: any, event: any) {
+        if (err) {
+          res.send(err);
         }
-        res.status(201).send('el evento no existe')
-        return
+        console.log(event);
+      });
+      res.status(200).send("new comment");
     }
-    else{
-        const events = await eventSchema.find({})
-        events.length
-            ? res.status(200).send(events)
-            : res.status(400).send('sin eventos')
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+export const findEvent = async (req: Request, res: Response) => {
+  const { name } = req.query;
+  try {
+    if (name) {
+      const event = await eventSchema.find({
+        name: { $regex: name, $options: "ig" },
+      });
+      if (event.length) {
+        res.status(200).send(event);
+        return;
+      }
+      res.status(201).send("el evento no existe");
+      return;
+    } else {
+      const events = await eventSchema.find({});
+      events.length
+        ? res.status(200).send(events)
+        : res.status(400).send("sin eventos");
     }
-    }catch(e){
-        res.status(400).send(e)
-        return
-    }
-}
+  } catch (e) {
+    res.status(400).send(e);
+    return;
+  }
+};
