e3270522a47ec43438485507d51e90a41f65efa4
Eric Daniele <ericdaniele34@gmail.com>
2022-10-15 19:30:26 -0300

    addpls

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,3 +1,4 @@
+import { findUserByEmail } from './../controllers/user.controller';
 import { Router } from "express";
 
 import { addUser, findUserByName, findUserById , deleteUser, addFriend } from "../controllers/user.controller";
@@ -10,6 +11,7 @@ router.route('')
     .get(findUserByName)
 
 router.route('/:id').get(findUserById)
+router.route('/email/:email').get(findUserByEmail)
 router.route('/follow')
     .post(addFriend)
 export default router;
