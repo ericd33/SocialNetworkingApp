4f8891f2e49f194cf1d8e8fc76c69ab0808325db
Eric Daniele <ericdaniele34@gmail.com>
2022-10-17 16:51:08 -0300

    rompiendo todo

---
+++ api/src/middleware/index.js
@@ -0,0 +1,22 @@
+const admin = require('../firebase/config')
+class Middleware {
+    async decodeToken(req, res, next) {
+
+        
+        try{
+            const token = req.headers.authorization.split(' ')[1];
+        const decodeValue = await admin.auth().verifyIdToken(token)
+        console.log(decodeValue)
+        if(decodeValue) {
+            return next();
+        }
+        return res.json({message:"Unauthorized."})
+    }
+    catch(e){
+        return res.json({message: 'Error'});
+    }
+    }
+
+}
+
+module.exports = new Middleware();
--- client/src/index.js
+++ client/src/index.js
@@ -5,25 +5,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store.js";
-import { Auth0Provider } from "@auth0/auth0-react";
-// import axios from 'axios'
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
-
+import {auth} from './firebase/config'
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(auth)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Auth0Provider
-          domain={process.env.REACT_APP_MY_DOMAIN}
-          clientId={process.env.REACT_APP_MY_CLIENTID}
-          redirectUri={window.location.origin}
-        >
           <App />
-        </Auth0Provider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
