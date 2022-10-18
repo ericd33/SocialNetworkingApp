fb52c181884d96956e4fda65739128c61d0a9299
Eric Daniele <ericdaniele34@gmail.com>
2022-10-18 14:17:55 -0300

    falto add

--- api/src/middleware/index.js
+++ api/src/middleware/index.js
@@ -1,18 +1,20 @@
 const admin = require('../firebase/config')
 class Middleware {
     async decodeToken(req, res, next) {
-
+        console.log('decoding token')
         
         try{
             const token = req.headers.authorization.split(' ')[1];
+
         const decodeValue = await admin.auth().verifyIdToken(token)
-        console.log(decodeValue)
         if(decodeValue) {
+            console.log('code value is valid, continuing')
             return next();
         }
         return res.json({message:"Unauthorized."})
     }
     catch(e){
+        console.log(e)
         return res.json({message: 'Error'});
     }
     }
