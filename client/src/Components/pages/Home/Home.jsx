a1ef787675d2ca0e390cc36f714d184a124b663d
Alejo Avendaño <alejoaven80@gmail.com>
2022-10-23 21:28:11 -0300

    link

--- client/src/Components/pages/Home/Home.jsx
+++ client/src/Components/pages/Home/Home.jsx
@@ -38,6 +38,7 @@ export default function Home() {
                       mb: 2,
                       mt: 1,
                       borderRadius:3
+                      
                     }
                   }
                   >
