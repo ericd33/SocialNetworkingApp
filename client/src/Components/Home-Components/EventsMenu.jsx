b29066986f53430b780ed034bcf7c0ee7d909326
Fede Rosales <federico.rosales.salva@gmail.com>
2022-10-26 14:17:14 -0300

    eventsToPosts

--- client/src/Components/Home-Components/EventsMenu.jsx
+++ client/src/Components/Home-Components/EventsMenu.jsx
@@ -5,7 +5,9 @@ import './EventsMenu.css'
 
 export default function EventsMenu() {
     return (
-        <Card sx={{width:200, bgcolor:'custom.dark', borderRadius:3}}>
+
+        window.location.href === `http://localhost:3000/home` ? (
+            <Card sx={{width:200, bgcolor:'custom.dark', borderRadius:3}}>
             <div className="eventsMenu">
                 <Typography sx={{fontFamily: 'Nunito', fontSize: 14,color:'primary.main'}} gutterBottom variant="h5" component="div">
                     - Events -
@@ -17,5 +19,20 @@ export default function EventsMenu() {
                 </Button></Link>
             </div>
         </Card>
-    )
-}
+            ) : (
+                <Card sx={{width:200, bgcolor:'custom.dark', borderRadius:3}}>
+                <div className="eventsMenu">
+                    <Typography sx={{fontFamily: 'Nunito', fontSize: 14,color:'primary.main'}} gutterBottom variant="h5" component="div">
+                        - Posts -
+                    </Typography>
+                </div>
+                <div className="buttons">
+                    <Link to={'/home'}><Button id='assistButton' sx={{bgcolor: 'secondary.main', color:grey[900], fontSize:11}} variant="contained">
+                        Show posts
+                    </Button></Link>
+                </div>
+            </Card>
+            ))}
+
+
+    
