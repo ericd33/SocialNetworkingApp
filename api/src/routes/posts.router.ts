73fec78f83c07324d0e9c73b40ccc90fb8732c08
Daniel Molina <dani_look1999@hotmail.com>
2022-10-26 10:13:06 -0500

    cloudinary

--- api/src/routes/posts.router.ts
+++ api/src/routes/posts.router.ts
@@ -7,6 +7,7 @@ import {
   findPostsByEmail,
   putPostComment,
   reports,
+  addfile
 } from "../controllers/posts.controller";
 const router = Router();
 
@@ -15,5 +16,6 @@ router.route("/:idPost").put(putPostLikes);
 router.route("/:postId/comment").put(putPostComment);
 router.route("/email/:email").get(findPostsByEmail);
 router.route("/report/post").post(reports)
+router.route('/file').post(addfile)
 
 export default router;
